a790e049d51f32bda726dbd94c00949600179e0f
Huaqing Jin <kevinjin0423@gmail.com>
2025-04-05 14:38:22 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Changes to be committed: 	modified:   jin_utils/misc.py 	new file:   jin_utils/rpy2_utils.py

--- jin_utils/misc.py
+++ jin_utils/misc.py
@@ -117,3 +117,75 @@ def save_pkl(fil, result, is_force=False, verbose=True):
         else:
             pass
 
+        
+def num2str(num, digits=3,
+             sci_bds=[0.01, 1000], dp="d", mp="m"):
+    """Return a string representation of a number with a given number of decimal places.
+    for save file name
+    if num is large 1e8, it will be converted to 1e8
+    args: 
+        - num (float): the number to be converted to a string
+        - digits (int): the number of significant digits
+        - sci_bds (list): the scientific bounds, smaller than sci_bds[0] or larger than sci_bds[1] will be converted to scientific notation
+        - dp (str): the decimal place separator
+        - mp (str): the minus sign
+    return: 
+        - strn (str): the string representation of the number
+    """
+    if num == 0:
+        return "0"
+
+    digits = int(digits)
+    is_negative = num < 0
+    num = np.abs(num)
+    if num >= sci_bds[1] or num <= sci_bds[0]:
+        strn = f"{num:.{digits}e}"
+    elif num.is_integer():
+        strn = f"{num:.0f}"
+    else: 
+        addn = 0
+        if num < 0.1:
+            log10num = np.log10(num)
+            addn = np.floor(np.abs(log10num))
+            if log10num.is_integer():
+                addn = addn -1
+            digits = digits + int(addn)
+        strn = f"{num:.{digits}f}"
+    strn = strn.lower()
+    strn = strn.replace(".", dp)
+    strn = strn.replace("-", mp)
+    strn = strn.replace("+", "")
+    # remove trailing zeros, if 8d0 then remove d0, if 1d800e10 then remove 00
+    if dp in strn:
+        if "e" not in strn:
+            strn = strn.rstrip("0")
+            if strn.endswith(dp):
+                strn = strn[:-1]
+        else: 
+            p1, p2 = strn.split("e")
+            p1 = p1.rstrip("0")
+            if p1.endswith(dp):
+                p1 = p1[:-1]
+            strn = p1 + "e" + p2
+    #remove the 0 in 1e08
+    if "em0" in strn:
+        p1, p2 = strn.split("em")
+        p2 = p2.lstrip("0")
+        strn = p1 + "em" + p2
+    if "e0" in strn:
+        p1, p2 = strn.split("e")
+        p2 = p2.lstrip("0")
+        strn = p1 + "e" + p2
+
+    if is_negative:
+        strn = mp + strn
+    return strn
+
+
+def str2num(s, dp="d", mp="m"): 
+    """Given a string representation of a number, return the number
+    """
+    s = s.replace(dp, ".")
+    s = s.replace(mp, "-")
+    return float(s)
+
