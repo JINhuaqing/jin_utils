4eef54504ad4d2f93473227955587064d47691da
Huaqing Jin <kevinjin0423@gmail.com>
2025-11-05 11:32:51 +0800

    fix some issue in __init__

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from jin_utils.version import __version__
 
 setup(
     name='jin_utils',
-    version='0.3',
+    version=__version__,
     author='Huaqing Jin',
     author_email='kevinjin0423@gmail.com',
     description='The package contains some useful functions for my daily work',
@@ -13,5 +14,7 @@ setup(
         'pandas>=1.0.0', 
         'easydict',
         'matplotlib',
+        'rpy2',
+        'pyyaml',
     ]
 )
